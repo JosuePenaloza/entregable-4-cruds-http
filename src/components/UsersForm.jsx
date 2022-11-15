97e1a31715bc2093cd78a206c1d517de71d8b236
Josue Peñaloza <91702875+JosuePenaloza@users.noreply.github.com>
2022-11-14 20:40:31 -0600

    7
    css

--- src/components/UsersForm.jsx
+++ src/components/UsersForm.jsx
@@ -2,13 +2,13 @@ import axios, { Axios } from 'axios';
 import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import useAlert from '../hooks/useAlert';
-//import useGetUsers from '../hooks/useGetUsers';
+import Swal from "sweetalert2";
 
 const UsersForm = ({ userSelected, getUsers}) => {
 
     const { register, handleSubmit, reset } = useForm(); 
-    const {confirmation,toUpdate,errorAlert} = useAlert()
-    //const {getUsers} = useGetUsers();
+    const {confirmation,errorAlert} = useAlert()
+
 
     useEffect(() => {
         if(userSelected){
@@ -20,12 +20,48 @@ const UsersForm = ({ userSelected, getUsers}) => {
     const submit = (data) =>{
         console.log(data)
         if(userSelected){
-            axios.put(`https://users-crud1.herokuapp.com/users/${userSelected.id}/`, data)
-            .then(() => getUsers())
-            .catch(error => {
-                errorAlert()
-                console.log('ERROR',error.response?.data)
-            } )
+            const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                  confirmButton: 'btn btn-success',
+                  cancelButton: 'btn btn-danger'
+                },
+                buttonsStyling: false
+            })
+              
+            swalWithBootstrapButtons.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, delete it!',
+                cancelButtonText: 'No, cancel!',
+                reverseButtons: true
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    swalWithBootstrapButtons.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                    )
+                    //////////////
+                    axios.put(`https://users-crud1.herokuapp.com/users/${userSelected.id}/`, data)
+                    .then(() => getUsers())
+                    .catch(error => {
+                        errorAlert()
+                        console.log('ERROR',error.response?.data)
+                    })
+
+                } else if (
+                    /* Read more about handling dismissals below */
+                    result.dismiss === Swal.DismissReason.cancel
+                ) {
+                    swalWithBootstrapButtons.fire(
+                        'Cancelled',
+                        'Your imaginary file is safe :)',
+                        'error'
+                    )
+                }
+            });       
         }else{
             axios.post(`https://users-crud1.herokuapp.com/users/`,data)
             .then(() =>{ 
